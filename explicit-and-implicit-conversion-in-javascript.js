c5a94c6a22bc7ab5cf7d01d370cb55dd11e9c53a
jasonrepoforlaunchcode <jasonrepoforlaunchcode@gmail.com>
2025-03-05 10:26:44 -0600

    Added explicit type conversions to avoid bugs and improve clarity. Wrote logic that uses implicit and explicit conversion to determine if a person has a hat and how many.

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -19,14 +19,38 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
-console.log("The result is: " + result);
+let result = Number("5") - 2;
+console.log("The result is: " + result);// added Number conversion function to be more precise and add clarity
 
-let isValid = Boolean("false");
+let isValid = Boolean(false);//Changed the string to boolean so that it would evaluate false
 if (isValid) {
-    console.log("This is valid!");
+    console.log("This is valid and true");
+}
+else {
+  console.log("This is valid and false!");
 }
 
 let age = "25";
-let totalAge = age + 5;
+let totalAge = Number(age) + 5; // added Number() function so that we are adding and not concatenating
 console.log("Total Age: " + totalAge);
+
+//implicit conversion of null to false
+let hat = null;
+
+if (hat) {
+  console.log("Check out my hat!")
+}
+else {
+  console.log("I have no hat, but check out my haircut!")
+}
+
+//explicit conversion of number to boolean
+
+let numberOfHats = 0;
+let hasAHat = Boolean(numberOfHats)
+if (hasAHat) {
+  console.log("I have at least one hat")
+}
+else {
+  console.log("I don't have a hat in the world")
+}
